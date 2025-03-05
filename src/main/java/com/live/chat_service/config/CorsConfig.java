5cc5987e4d8fd19589d6d56e07726da24ef31475
Sanjay Hanumanth <sanjayhanumanth.t@coherent.in>
2025-03-05 15:20:31 +0530

    Cors Config changes

--- src/main/java/com/live/chat_service/config/CorsConfig.java
+++ src/main/java/com/live/chat_service/config/CorsConfig.java
@@ -11,10 +11,9 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOriginPatterns("*") // Use this instead of allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
-
     }
 }
