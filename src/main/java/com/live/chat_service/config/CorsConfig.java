26ae89407cba005fa998be4fd5656f3cd2d9f258
Ranjithkumar <ranjithkumar.murugaraj@coherent.in>
2025-03-06 16:38:26 +0530

    cors fixed

--- src/main/java/com/live/chat_service/config/CorsConfig.java
+++ src/main/java/com/live/chat_service/config/CorsConfig.java
@@ -11,8 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("http://localhost:4230")
-//                .allowedOriginPatterns("*") // Use this instead of allowedOrigins("*")
+                .allowedOriginPatterns("*") // Use this instead of allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
