3f08352c68c0ed307c1d660c1740702c551274ae
sivaprakash sakthivel <sivaprakash2798@gmail.com>
2025-03-07 16:41:10 +0530

    Message list api

--- src/main/java/com/live/chat_service/repository/ChatMessageRepository.java
+++ src/main/java/com/live/chat_service/repository/ChatMessageRepository.java
@@ -1,9 +1,7 @@
 package com.live.chat_service.repository;
 
-import com.live.chat_service.dto.ChatDTO;
 import com.live.chat_service.model.ChatMessage;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,9 +9,7 @@ import java.util.List;
 @Repository
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
 
-    List<ChatMessage> findBySender_IdOrReceiver_IdOrderByTimestampAsc(Long senderId, Long receiverId);
+    List<ChatMessage> findBySenderIdAndReceiverIdOrderByTimestampAsc(Long senderId, Long receiverId);
 
-//    @Query("SELECT m FROM ChatMessage m WHERE m.sender.id = :senderId OR m.receiver.id = :receiverId ORDER BY m.timestamp ASC")
-//    List<ChatMessage> findBySenderReceiverId(Long senderId, Long receiverId);
 
 }
